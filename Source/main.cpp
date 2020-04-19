a667ed4f965c70169de77231ca2813fbb7dc6464
nNown <wojciechmar2000@gmail.com>
2020-04-19 13:57:37 +0200

    Game singleton and changes to frame refreshing started

--- Source/main.cpp
+++ Source/main.cpp
@@ -7,28 +7,18 @@
 #include <vector>
 #include <windows.h>
 
-// VK_LEFT   A 
-// 0x25      0x41
-
-// VK_UP     W
-// 0x26      0x57
-
-// VK_RIGHT  D
-// 0x27      0x44
-
-// VK_DOWN   S
-// 0x28      0x53
-
 #define PressedKeyMask 0x8000
 
 void GetInput(PlayerEntity* player);
 void ManageFood(BoardEntity& board);
+void ShowConsoleCursor(bool showFlag);
 
 int main() {
-    std::srand(std::time(nullptr));
-    std::array<unsigned int, 2> dimensions = { 10, 25 };
-    BoardEntity board = BoardEntity(dimensions, PlayerEntity('X', { 6, 15 }), FoodEntity({1 + rand() % (dimensions[0] - 2), 1 + rand() % (dimensions[1] - 2)}));
+    ShowConsoleCursor(false);
 
+    std::srand(std::time(nullptr));
+    std::array<unsigned int, 2> dimensions = { 20, 35 };
+    BoardEntity board = BoardEntity(dimensions, PlayerEntity('X', { 6, 15 }), FoodEntity({1 + rand() % (dimensions[0] - 2), 1 + rand() % (dimensions[1] - 2)}));
 
     while(!board.Player()->GameState()) {
         GetInput(board.Player());
@@ -56,4 +46,14 @@ void ManageFood(BoardEntity& board) {
         board.Player()->AddBodyPart(board.Dimensions());
         board.Food()->SetPosition({1 + rand() % (board.Dimensions()[0] - 2), 1 + rand() % (board.Dimensions()[1] - 2)});
     }
+}
+
+void ShowConsoleCursor(bool showFlag) {
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_CURSOR_INFO     cursorInfo;
+
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag; // set the cursor visibility
+    SetConsoleCursorInfo(out, &cursorInfo);
 }
